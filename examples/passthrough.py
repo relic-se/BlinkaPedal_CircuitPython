73f88535535c2a8fee35c74c324fffacf325949e
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2026-07-31 18:14:20 -0500

    Move passthrough into separate examples folder

--- examples/passthrough.py
+++ examples/passthrough.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: GPLv3
 
 from blinka_pedal import BlinkaPedal
-import programs
 
 # Initialize Hardware
 pedal = BlinkaPedal()
@@ -15,7 +14,6 @@ pedal.audio_out.play(
 
 while True:
     pedal.update()
-    programs.update(pedal)
 
     if pedal.right_button.released:
         pedal.bypass = not pedal.bypass
