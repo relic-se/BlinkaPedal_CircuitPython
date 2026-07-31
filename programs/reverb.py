8072631d42306ec221d330ca0f0db5e406f8e030
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2026-07-31 11:11:19 -0500

    Add reverb effect

---
+++ programs/reverb.py
@@ -0,0 +1,71 @@
+# SPDX-FileCopyrightText: 2026 Cooper Dalrymple (@relic-se)
+#
+# SPDX-License-Identifier: GPLv3
+
+from audiofreeverb import Freeverb
+from audiofilters import Filter
+from synthio import Biquad, FilterMode
+
+from blinka_pedal import BlinkaPedal
+import programs
+
+# Constants
+LPF_FILTER_FREQ = 4000  # hz
+HPF_FILTER_FREQ = 800  # hz
+
+BUFFER_SIZE     = 2048  # bytes
+
+# Initialize Hardware
+pedal = BlinkaPedal()
+
+# Audio Objects
+reverb_effect = Freeverb(
+    roomsize=0.0,
+    damp=0.0,
+    mix=1.0,
+
+    buffer_size=BUFFER_SIZE,
+    **pedal.audiosample_args,
+)
+
+filter_effect = Filter(
+    filter=(
+        Biquad(FilterMode.LOW_PASS, 20000),
+        Biquad(FilterMode.HIGH_PASS, 20),
+    ),
+    mix=1.0,
+
+    buffer_size=BUFFER_SIZE,
+    **pedal.audiosample_args,
+)
+
+# Audio Chain
+pedal.audio_out.play(
+    filter_effect.play(
+        reverb_effect.play(
+            pedal.audio_in
+        )
+    )
+)
+
+infinite = False
+while True:
+    pedal.update()
+    programs.update(pedal)
+
+    roomsize, reverb_effect.damp, pedal.mix = pedal.pots
+
+    lpf, hpf = filter_effect.filter
+    lpf.frequency = 20000 if pedal.left_switch.value else LPF_FILTER_FREQ
+    hpf.frequency = 20 if pedal.right_switch.value else HPF_FILTER_FREQ
+
+    if pedal.left_button.pressed:
+        infinite = True
+    elif pedal.left_button.released:
+        infinite = False
+    
+    reverb_effect.roomsize = 1.0 if infinite else roomsize
+
+    if pedal.right_button.released:
+        pedal.bypass = not pedal.bypass
+        pedal.led = not pedal.bypass
