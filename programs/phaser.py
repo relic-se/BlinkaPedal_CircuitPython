7ec3a7e98a334135675f57bda513bd6b11f1cbd9
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2026-08-03 15:34:08 -0500

    Improve phaser controls

--- programs/phaser.py
+++ programs/phaser.py
@@ -9,14 +9,14 @@ from blinka_pedal import BlinkaPedal
 import programs
 
 # Constants
-STAGES_INCREMENT = 2
+STAGES_INCREMENT = 8
 
 MIN_FREQUENCY = 200
 MAX_FREQUENCY = 2000
 MIN_DEPTH = 0.25
 
-MIN_RATE = 0.1
-MAX_RATE = 8.0
+MIN_RATE = 0.05
+MAX_RATE = 4.0
 
 MIN_FEEDBACK = 0.5
 MAX_FEEDBACK = 1.0
@@ -56,7 +56,7 @@ while True:
     pedal.led = (lfo.value - MIN_FREQUENCY) / (MAX_FREQUENCY - MIN_FREQUENCY) * (not pedal.bypass)
 
     pots = pedal.pots
-    lfo.rate = pots[0] * (MAX_RATE - MIN_RATE) + MIN_RATE
+    lfo.rate = pow(pots[0], 2) * (MAX_RATE - MIN_RATE) + MIN_RATE
     lfo.scale = (pots[1] * (1 - MIN_DEPTH) + MIN_DEPTH) * (MAX_FREQUENCY - MIN_FREQUENCY) / 2
     effect.feedback = pots[2] * (MAX_FEEDBACK - MIN_FEEDBACK) + MIN_FEEDBACK
     
