2aca29e1879b303ab968ab173c51465cfa9f683b
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2026-08-03 15:32:59 -0500

    Simplify passthrough

--- examples/passthrough.py
+++ examples/passthrough.py
@@ -5,7 +5,10 @@
 from blinka_pedal import BlinkaPedal
 
 # Initialize Hardware
-pedal = BlinkaPedal()
+pedal = BlinkaPedal(
+    mix=1.0,
+    level=1.0,
+)
 
 # Audio Chain
 pedal.audio_out.play(
@@ -18,7 +21,6 @@ while True:
     if pedal.right_button.released:
         pedal.bypass = not pedal.bypass
         pedal.led = not pedal.bypass
-
-    pedal.mix, pedal.level, _ = pedal.pots
-
-    pedal.codec.adc_loopback = not pedal.left_switch.value
+        
+    if pedal.left_switch.rose or pedal.left_switch.fell:
+        pedal.codec.adc_loopback = not pedal.left_switch.value
